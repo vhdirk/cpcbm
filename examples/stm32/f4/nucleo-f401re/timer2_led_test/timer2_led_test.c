aac2017d9e521abd01f7342acf955f491bc13494
adichell <angelo.dichello@gmail.com>
2016-02-16 06:47:35 +0100

    Removed unused targets. Added a test with timer2 that blinks a the user led.

---
+++ examples/stm32/f4/nucleo-f401re/timer2_led_test/timer2_led_test.c
@@ -0,0 +1,41 @@
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
+//#include <libopencm3/cm3/nvic.h>
+
+//#include <libopencmsis/core_cm3.h>
+
+int main(void)
+{
+	/* Enable TIM2 interrupt. */
+//	 	nvic_enable_irq(NVIC_TIM2_IRQ);
+
+
+		rcc_periph_clock_enable(RCC_TIM2);
+		gpio_set_output_options(GPIOA, GPIO_OTYPE_PP,
+					GPIO_OSPEED_50MHZ, GPIO5);
+
+		timer_reset(TIM2);
+		timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT_MUL_2,
+				   TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+		timer_set_oc_mode(TIM2, TIM_OC1, TIM_OCM_TOGGLE);
+		timer_enable_oc_output(TIM2, TIM_OC1);
+		/* Set the capture compare value for OC1. */
+			timer_set_oc_value(TIM2, TIM_OC1, 100);
+		timer_set_period(TIM2, 10000000);
+		timer_enable_counter(TIM2);
+
+		/* Enable GPIOA clock. */
+		/* Using API functions: */
+		rcc_periph_clock_enable(RCC_GPIOA);
+
+		/* Set GPIO5 (in GPIO port A) to 'output push-pull'. */
+		/* Using API functions: */
+		gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO5);
+
+		gpio_set_af(GPIOA, 0x1, GPIO5);
+
+	while (1);
+
+}
